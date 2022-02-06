769ec8df6c4d875d7b6702b2569ee53facb246cc
Dalton <slattery.da@gmail.com>
2022-02-05 19:27:37 -0600

    works, but not exact name for tests

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({items, setItems}) {  
+  const [itemName, setItemName] = useState('')
+  const [itemCategory, setItemCategory] = useState('Produce')
+
+  function handleItemChange(e){
+    setItemName(e.target.value)
+  }
+
+  function handleCategoryChange(e){
+    setItemCategory(e.target.value)
+  }
+
+  function onItemFormSubmit(e){
+    e.preventDefault()
+    
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory
+    }
+      const newArr = [...items, newItem]
+      setItems(newArr)
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={onItemFormSubmit} className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input onChange={handleItemChange} type="text" name="name" />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select onChange={handleCategoryChange} name="category">
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
