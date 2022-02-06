054a1979630502391ddc9af48a2ecd93441d3433
Dalton <slattery.da@gmail.com>
2022-02-05 19:27:21 -0600

    commit tree ede95710c922301345535353b99674f31910ff14

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,24 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, setItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('')
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  function onSearchChange(e){
+    setSearch(e.target.value)
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value)
+  }
 
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items.filter((item) => selectedCategory === "All" || item.category === selectedCategory).filter((item) => item.name.toLowerCase().includes(search.toLocaleLowerCase()))
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm items={items} setItems={setItems}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
